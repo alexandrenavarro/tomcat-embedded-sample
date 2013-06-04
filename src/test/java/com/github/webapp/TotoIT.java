1767d394c37589fc88981e7acf95bc31b6f15a02
alexandrenavarro <alexandre.j.navarro@gmail.com>
2013-06-05 00:20:08 +0200

    Add Integration Test

---
+++ src/test/java/com/github/webapp/TotoIT.java
@@ -0,0 +1,25 @@
+package com.github.webapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * <p>TotoIT. </p>
+ *
+ * @author anavarro - Jun 5, 2013
+ *
+ */
+public class TotoIT {
+
+    /**
+     * test.
+     *
+     */
+    @Test
+    public void test() {
+        assertTrue(true);
+        
+    }
+
+}
